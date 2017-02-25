f74a864f720ad45d891d9a5e1d54474f4d295d3d
simonry14 <simonpetermiyingo@gmail.com>
2017-02-25 20:36:53 +0300

    Update app.js

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const app = express();
 // [START hello_world]
 // Say hello!
 app.get('/', (req, res) => {
-  res.status(200).send('Hello, world!');
+  res.status(200).send('Hello, Simon!');
 });
 // [END hello_world]
 
